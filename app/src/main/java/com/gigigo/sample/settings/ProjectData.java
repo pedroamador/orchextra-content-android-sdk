fe63cd1872cae4c3192131d5b8f463be45b6e752
Manuel Rebollo <mrebollob@gmail.com>
2017-11-03 13:04:51 +0100

    Update: Change library build type

--- app/src/main/java/com/gigigo/sample/settings/ProjectData.java
+++ app/src/main/java/com/gigigo/sample/settings/ProjectData.java
@@ -1,6 +1,7 @@
 package com.gigigo.sample.settings;
 
 import android.support.annotation.NonNull;
+import com.gigigo.sample.BuildConfig;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -32,33 +33,40 @@ public final class ProjectData {
 
     List<ProjectData> projectDataList = new ArrayList<>();
 
-    projectDataList.add(new ProjectData("Default", "33ecdcbe03d60cb530e6ae13a531a3c9cf3c150e",
-        "be772ab61e2571230c596aa95237cc618023befb"));
+    if (BuildConfig.BUILD_TYPE == "release") {
 
-    projectDataList.add(
-        new ProjectData("[PRO][ES] ORCHEXTRA DEMO", "9d9f74d0a9b293a2ea1a7263f47e01baed2cb0f3",
-            "6a4d8072f2a519c67b0124656ce6cb857a55276a"));
+      projectDataList.add(new ProjectData("Default", "33ecdcbe03d60cb530e6ae13a531a3c9cf3c150e",
+          "be772ab61e2571230c596aa95237cc618023befb"));
 
-    projectDataList.add(
-        new ProjectData("[UAT] WOAH MARKETS", "ef08c4dccb7649b9956296a863db002a68240be2",
-            "6bc18c500546f253699f61c11a62827679178400"));
+      projectDataList.add(
+          new ProjectData("[PRO][ES] ORCHEXTRA DEMO", "9d9f74d0a9b293a2ea1a7263f47e01baed2cb0f3",
+              "6a4d8072f2a519c67b0124656ce6cb857a55276a"));
 
-    projectDataList.add(
-        new ProjectData("Orchextra Demo - {{staging}}", "3b1f7352079beaede1b234e798bdf05a0266a0ff",
-            "df22059fd20c5b4b6e6465ab6b044628ea9a0726"));
+      projectDataList.add(
+          new ProjectData("[UAT] WOAH MARKETS", "ef08c4dccb7649b9956296a863db002a68240be2",
+              "6bc18c500546f253699f61c11a62827679178400"));
+    } else if (BuildConfig.BUILD_TYPE == "quality") {
 
-    projectDataList.add(
-        new ProjectData("ANDROID SDK - {{staging}}", "34a4654b9804eab82aae05b2a5f949eb2a9f412c",
-            "2d5bce79e3e6e9cabf6d7b040d84519197dc22f3"));
+      projectDataList.add(new ProjectData("Orchextra Demo - {{staging}}",
+          "3b1f7352079beaede1b234e798bdf05a0266a0ff", "df22059fd20c5b4b6e6465ab6b044628ea9a0726"));
+    } else {
+
+      projectDataList.add(new ProjectData("Orchextra Demo - {{staging}}",
+          "3b1f7352079beaede1b234e798bdf05a0266a0ff", "df22059fd20c5b4b6e6465ab6b044628ea9a0726"));
+
+      projectDataList.add(
+          new ProjectData("ANDROID SDK - {{staging}}", "34a4654b9804eab82aae05b2a5f949eb2a9f412c",
+              "2d5bce79e3e6e9cabf6d7b040d84519197dc22f3"));
+    }
 
     return projectDataList;
   }
 
   @NonNull public static String getDefaultApiKey() {
-    return getDefaultProjectDataList().get(3).apiKey;
+    return getDefaultProjectDataList().get(0).apiKey;
   }
 
   @NonNull public static String getDefaultApiSecret() {
-    return getDefaultProjectDataList().get(3).apiSecret;
+    return getDefaultProjectDataList().get(0).apiSecret;
   }
 }
