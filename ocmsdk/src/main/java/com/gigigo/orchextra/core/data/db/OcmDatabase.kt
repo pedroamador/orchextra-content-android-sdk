7df7c5d66e1ca046998954b048d981ceb06992e3
Claudia Brito <claudia.brito@gigigo.com>
2018-02-05 15:43:50 +0100

    Update: move to constant

--- ocmsdk/src/main/java/com/gigigo/orchextra/core/data/db/OcmDatabase.kt
+++ ocmsdk/src/main/java/com/gigigo/orchextra/core/data/db/OcmDatabase.kt
@@ -7,27 +7,30 @@ import android.content.Context
 import com.gigigo.orchextra.core.data.vo.*
 
 @Database(
-        entities = [(Menu::class), (Section::class), (MenuWithSections::class),
-            (SectionRender::class), (SectionRenderWithInfo::class), (SectionShare::class),
-            (SectionView::class), (Tag::class), (CustomProperty::class), (Element::class),
-            (FederatedAuthorization::class), (FederatedAuthorizationKeys::class),
-            (FederatedAuthorizationWithKeys::class), (SectionWithInfo::class)],
-        version = 1,
-        exportSchema = false
+    entities = [(Menu::class), (Section::class), (MenuWithSections::class),
+      (SectionRender::class), (SectionRenderWithInfo::class), (SectionShare::class),
+      (SectionView::class), (Tag::class), (CustomProperty::class), (Element::class),
+      (FederatedAuthorization::class), (FederatedAuthorizationKeys::class),
+      (FederatedAuthorizationWithKeys::class), (SectionWithInfo::class)],
+    version = 1,
+    exportSchema = false
 )
 abstract class OcmDatabase : RoomDatabase() {
-    companion object {
-        fun create(context: Context, useInMemory : Boolean, dbName : String = "ocm.db"): OcmDatabase {
-            val databaseBuilder = if(useInMemory) {
-                Room.inMemoryDatabaseBuilder(context, OcmDatabase::class.java)
-            } else {
-                Room.databaseBuilder(context, OcmDatabase::class.java, dbName)
-            }
-            return databaseBuilder
-                    .fallbackToDestructiveMigration()
-                    .build()
-        }
-    }
+  companion object {
+    private val DEFAULT_DATABASE_NAME = "ocm.db"
 
-    abstract fun menus(): MenuDao
+    fun create(context: Context, useInMemory: Boolean,
+        dbName: String = DEFAULT_DATABASE_NAME): OcmDatabase {
+      val databaseBuilder = if (useInMemory) {
+        Room.inMemoryDatabaseBuilder(context, OcmDatabase::class.java)
+      } else {
+        Room.databaseBuilder(context, OcmDatabase::class.java, dbName)
+      }
+      return databaseBuilder
+          .fallbackToDestructiveMigration()
+          .build()
+    }
+  }
+
+  abstract fun menus(): MenuDao
 }
